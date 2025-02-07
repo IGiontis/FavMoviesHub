32bc7acdaeb1f57d86589ca2f0773d19ebdf16d4
IGiontis <iliasgiontis@gmail.com>
2025-02-07 18:44:50 +0200

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Layout from "./components/Layout";
 import useTheme from "./hooks/useTheme";
 import "./styles/app.css";
 import store from "./redux/store";
-import { Provider, useDispatch } from "react-redux";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { setUser, logout } from "./redux/authSlice";
-
-function AuthListener() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const auth = getAuth();
-    
-    onAuthStateChanged(auth, (user) => {
-      console.log("🔥 Checking Auth State...");
-      console.log("🔍 User from Firebase Auth:", user);
-
-      if (user) {
-        console.log("✅ User is logged in:", user.email);
-        dispatch(setUser({ uid: user.uid, email: user.email }));
-      } else {
-        console.log("❌ No user logged in.");
-        dispatch(logout());
-      }
-    });
-  }, [dispatch]);
-
-  return null;
-}
+import { Provider } from "react-redux";
 
 function App() {
   const { isDarkMode, toggleTheme } = useTheme();
@@ -38,9 +12,9 @@ function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <AuthListener />
         <Layout toggleTheme={toggleTheme} isDarkMode={isDarkMode}>
           <AppRoutes />
+         
         </Layout>
       </BrowserRouter>
     </Provider>
