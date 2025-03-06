827eba3bfe5159bca586ebdb4562a2c846c724a5
IGiontis <iliasgiontis@gmail.com>
2025-03-06 10:29:39 +0200

    keep user signed In

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,8 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Load user from localStorage
+const userFromStorage = JSON.parse(localStorage.getItem("user"));
+
 const initialState = {
-  user: null,
-  isAuthenticated: false,
+  user: userFromStorage || null,
+  isAuthenticated: !!userFromStorage,
 };
 
 const authSlice = createSlice({
@@ -12,14 +15,15 @@ const authSlice = createSlice({
     setUser: (state, action) => {
       state.user = action.payload;
       state.isAuthenticated = true;
-     
+      localStorage.setItem("user", JSON.stringify(action.payload)); // Store user in localStorage
     },
     logout: (state) => {
       state.user = null;
       state.isAuthenticated = false;
+      localStorage.removeItem("user"); // Clear localStorage on logout
     },
   },
 });
 
-export const {setUser,logout}=authSlice.actions
-export default authSlice.reducer
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
