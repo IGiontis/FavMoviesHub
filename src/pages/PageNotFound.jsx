60c4b643c24854e754dd7123803130e57e9ce08f
IGiontis <iliasgiontis@gmail.com>
2025-03-14 14:41:49 +0200

    page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
 
 const PageNotFound = () => {
   const navigate = useNavigate();
@@ -7,12 +8,19 @@ const PageNotFound = () => {
     navigate("/");
   };
   return (
-    <div>
-      PageNotFound
-      <button type="button" onClick={handleGoHome} className="btn btn-primary">
-        Go to Home
-      </button>
-    </div>
+    <Container fluid className="d-flex justify-content-center mt-5">
+      <Row>
+        <Col className="d-flex align-items-center">
+        <h6 className="mb-0 me-4">
+
+          PageNotFound
+        </h6>
+          <button type="button" onClick={handleGoHome} className="btn btn-primary">
+            Go to Home
+          </button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
