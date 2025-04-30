ad7cb76c7cbb02555e73a02174613459b2671112
IGiontis <iliasgiontis@gmail.com>
2025-04-30 11:11:53 +0300

    loader before javascript

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+
+requestAnimationFrame(() => {
+  const preloader = document.getElementById("preloader");
+  if (preloader) {
+    preloader.classList.add("fade-out");
+    setTimeout(() => preloader.remove(), 300);
+  }
+});
